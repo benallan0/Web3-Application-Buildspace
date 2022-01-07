925d362d77a2e261455d1b308f074c0c1aeaace0
Ben Allan <87513029+benallan0@users.noreply.github.com>
2022-01-07 15:55:17 +1300

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
